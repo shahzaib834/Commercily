c805202e8b582030811c0e86c016879566063fea
Shahzaib834 <virgo834@gmail.com>
2023-05-27 21:23:09 +0500

    created the basic frontend layout

--- CLIENT/src/app/app-routing.module.ts
+++ CLIENT/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeScreen } from './Screens/homeScreen/home-screen.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: HomeScreen },
+  { path: '**', redirectTo: 'home' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- CLIENT/src/app/app.module.ts
+++ CLIENT/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeScreen } from './Screens/homeScreen/home-screen.component';
+import { NavComponent } from './Components/Nav/navbar.component';
+import { FooterComponent } from './Components/Footer/footer.component';
+import { SideBarComponent } from './Components/Sidebar/sidebar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    SideBarComponent,
+    HomeScreen,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
